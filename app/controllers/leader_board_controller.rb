99c03486e09d14597ad1bb9aed6dc66a43b1bdbd
Karl Baum <karl.baum@gmail.com>
2016-01-15 11:18:24 -0500

    formatting and link

--- app/controllers/leader_board_controller.rb
+++ app/controllers/leader_board_controller.rb
@@ -1,6 +1,10 @@
 class LeaderBoardController < ApplicationController
-  def show
-    @stats = $github_client.contributors_stats('viewthespace/viewthespace').
+
+  helper_method :stats
+
+  def stats
+    @stats ||= $github_client.contributors_stats('viewthespace/viewthespace').
       collect{|stat| Hashie::Mash.new({author: stat.author, deletions: stat.weeks.last.d})}.sort_by(&:deletions).reverse
   end
+
 end
