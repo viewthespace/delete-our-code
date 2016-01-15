b82469f882b2d9d07337e850b8375ff88a41398e
Karl Baum <karl.baum@gmail.com>
2016-01-15 10:00:41 -0500

    basic deleting our code leader board is working

--- app/controllers/leader_board_controller.rb
+++ app/controllers/leader_board_controller.rb
@@ -1,6 +1,6 @@
 class LeaderBoardController < ApplicationController
   def show
     @stats = $github_client.contributors_stats('viewthespace/viewthespace').
-      collect{|stat| Hashie::Mash.new({author: stat.author, deletions: stat.weeks.last.d})}.sort_by(&:deletions)
+      collect{|stat| Hashie::Mash.new({author: stat.author, deletions: stat.weeks.last.d})}.sort_by(&:deletions).reverse
   end
 end
